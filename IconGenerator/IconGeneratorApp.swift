92b1e762f377a68b837c60761337763d16ac1473
Kirill Zaborsky <avto.oil@gmail.com>
2022-01-28 11:40:18 +0700

    Initial Commit

---
+++ IconGenerator/IconGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconGeneratorApp.swift
+//  IconGenerator
+//
+//  Created by Кирилл Заборский on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct IconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
